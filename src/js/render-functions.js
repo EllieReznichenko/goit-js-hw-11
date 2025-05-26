669728c13235f9dc76d01542d03aad29dfae8858
Elina Reznichenko <missflash96@gmail.com>
2025-05-27 00:32:13 +0300

    update

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -13,27 +13,27 @@ export function createGallery(images) {
   const markup = images
     .map(image => {
       return `
-        <a class="gallery__item" href="${image.largeImageURL}">
-          <div class="photo-card">
-            <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
-            <div class="info">
-              
-  <div class="info-labels">
-    <p><b>Comments</b></p>
-    <p><b>Likes</b></p>
-    <p><b>Views</b></p>
-    <p><b>Downloads</b></p>
-  </div>
-  <div class="info-values">
-    <p>${image.comments}</p>
-    <p>${image.likes}</p>
-    <p>${image.views}</p>
-    <p>${image.downloads}</p>
-  </div>
-</div>
+        <li class="gallery__item">
+          <a href="${image.largeImageURL}">
+            <div class="photo-card">
+              <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+              <div class="info">
+                <div class="info-labels">
+                  <p><b>Comments</b></p>
+                  <p><b>Likes</b></p>
+                  <p><b>Views</b></p>
+                  <p><b>Downloads</b></p>
+                </div>
+                <div class="info-values">
+                  <p>${image.comments}</p>
+                  <p>${image.likes}</p>
+                  <p>${image.views}</p>
+                  <p>${image.downloads}</p>
+                </div>
+              </div>
             </div>
-          
-        </a>
+          </a>
+        </li>
       `;
     })
     .join('');
